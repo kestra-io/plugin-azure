dd70bf3cde8ed57059263d3b74b6962f673e04d8
Ludovic DEHON <tchiot.ludo@gmail.com>
2023-03-07 12:36:35 +0100

    docs(tasks): invalid task id on outputs

--- src/main/java/io/kestra/plugin/azure/batch/models/Task.java
+++ src/main/java/io/kestra/plugin/azure/batch/models/Task.java
@@ -92,7 +92,7 @@ public class Task {
         description = "List of key that will generate temporary files.\n" +
             "On the command, just can use with special variable named `outputFiles.key`.\n" +
             "If you add a files with `[\"first\"]`, you can use the special vars `echo 1 >> {[ outputFiles.first }}`" +
-            " and you used on others tasks using `{{ outputs.task-id.files.first }}`"
+            " and you used on others tasks using `{{ outputs.taskId.outputFiles.first }}`"
     )
     @PluginProperty(dynamic = false)
     List<String> outputFiles;

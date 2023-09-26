422331e119f51ebaa9517aadf50dcfe9a4787f69
Martin <mguibert@kestra.io>
2023-09-26 08:55:49 +0200

    chore(cli): add a service principal to github and use it to test cli (#47)
    close #41

--- src/test/java/io/kestra/plugin/azure/cli/AzCLITest.java
+++ src/test/java/io/kestra/plugin/azure/cli/AzCLITest.java
@@ -5,6 +5,7 @@ import io.kestra.core.runners.RunContextFactory;
 import io.kestra.core.utils.IdUtils;
 import io.kestra.core.utils.TestsUtils;
 import io.kestra.plugin.scripts.exec.scripts.models.ScriptOutput;
+import io.micronaut.context.annotation.Value;
 import io.micronaut.test.extensions.junit5.annotation.MicronautTest;
 import jakarta.inject.Inject;
 import org.junit.jupiter.api.Test;
@@ -14,9 +15,17 @@ import java.util.Map;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.core.AllOf.allOf;
 
-@MicronautTest
+@MicronautTest(environments = "sp")
 public class AzCLITest {
+    @Value("${kestra.variables.globals.azure.sp.username}")
+    protected String username;
+    @Value("${kestra.variables.globals.azure.sp.secret}")
+    protected String secret;
+    @Value("${kestra.variables.globals.azure.sp.tenant}")
+    String tenant;
+
     @Inject
     private RunContextFactory runContextFactory;
 
@@ -26,18 +35,18 @@ public class AzCLITest {
         String envValue = "MY_VALUE";
 
         AzCLI execute = AzCLI.builder()
-                .id(IdUtils.create())
-                .type(AzCLI.class.getName())
-                .env(Map.of("{{ inputs.envKey }}", "{{ inputs.envValue }}"))
-                .commands(List.of(
-                        "echo \"::{\\\"outputs\\\":{\\\"{{ inputs.outputName }}\\\":\\\"$" + envKey + "\\\"}}::\""
-                ))
-                .build();
+            .id(IdUtils.create())
+            .type(AzCLI.class.getName())
+            .env(Map.of("{{ inputs.envKey }}", "{{ inputs.envValue }}"))
+            .commands(List.of(
+                "echo \"::{\\\"outputs\\\":{\\\"{{ inputs.outputName }}\\\":\\\"$" + envKey + "\\\"}}::\""
+            ))
+            .build();
 
         RunContext runContext = TestsUtils.mockRunContext(runContextFactory, execute, Map.of(
-                "envKey", envKey,
-                "envValue", envValue,
-                "outputName", "customEnv"
+            "envKey", envKey,
+            "envValue", envValue,
+            "outputName", "customEnv"
         ));
 
         ScriptOutput runOutput = execute.run(runContext);
@@ -47,28 +56,31 @@ public class AzCLITest {
         assertThat(execute.getLoginCommands(runContext), empty());
 
         execute = AzCLI.builder()
-                .id(IdUtils.create())
-                .type(AzCLI.class.getName())
-                .username("{{ inputs.myUser }}")
-                .password("{{ inputs.myPassword }}")
-                .tenant("{{ inputs.myTenant }}")
-                .servicePrincipal(true)
-                .build();
+            .id(IdUtils.create())
+            .type(AzCLI.class.getName())
+            .username("{{ inputs.myUser }}")
+            .password("{{ inputs.myPassword }}")
+            .tenant("{{ inputs.myTenant }}")
+            .servicePrincipal(true)
+            .commands(List.of("az keyvault list"))
+            .build();
 
-        String username = "someUser";
-        String password = "somePassword";
-        String tenant = "someTenant";
         runContext = TestsUtils.mockRunContext(runContextFactory, execute, Map.of(
-                "myUser", username,
-                "myPassword", password,
-                "myTenant", tenant
+            "myUser", username,
+            "myPassword", secret,
+            "myTenant", tenant
         ));
         assertThat(execute.getLoginCommands(runContext), allOf(
-                iterableWithSize(1),
-                hasItem("az login -u " + username +
-                        " -p " + password +
-                        " --tenant " + tenant +
-                        " --service-principal")
+            iterableWithSize(1),
+            hasItem("az login -u " + username +
+                " -p " + secret +
+                " --tenant " + tenant +
+                " --service-principal")
         ));
+
+        runOutput = execute.run(runContext);
+
+        assertThat(runOutput.getExitCode(), is(0));
+
     }
 }

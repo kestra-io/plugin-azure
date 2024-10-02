3eb2bf7ecb35738bcc6f913312bf7eb4769be40c
Mathieu Gabelle <mgabelle@kestra.io>
2024-10-02 09:58:13 +0200

    feat(functions): Add Azure functions to Azure plugin
    add unit tests disabled by default
    
    add http client to trigger an Azure function

---
+++ src/main/java/io/kestra/plugin/azure/function/HttpTrigger.java
@@ -0,0 +1,122 @@
+package io.kestra.plugin.azure.function;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonSyntaxException;
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.annotations.PluginProperty;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.tasks.Output;
+import io.kestra.core.models.tasks.RunnableTask;
+import io.kestra.core.models.tasks.Task;
+import io.kestra.core.runners.DefaultRunContext;
+import io.kestra.core.runners.RunContext;
+import io.micronaut.core.type.Argument;
+import io.micronaut.http.HttpMethod;
+import io.micronaut.http.HttpRequest;
+import io.micronaut.http.HttpResponse;
+import io.micronaut.http.client.DefaultHttpClientConfiguration;
+import io.micronaut.http.client.HttpClient;
+import io.micronaut.http.client.exceptions.HttpClientResponseException;
+import io.micronaut.http.client.netty.DefaultHttpClient;
+import io.micronaut.http.client.netty.NettyHttpClientFactory;
+import io.micronaut.http.codec.MediaTypeCodecRegistry;
+import io.micronaut.http.uri.UriTemplate;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import reactor.core.publisher.Mono;
+
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Map;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+public class HttpTrigger extends Task implements RunnableTask<HttpTrigger.Output> {
+    private static final Duration HTTP_READ_TIMEOUT = Duration.ofSeconds(60);
+    private static final NettyHttpClientFactory FACTORY = new NettyHttpClientFactory();
+
+    @Schema(title = "Http method")
+    @NotNull
+    protected Property<String> httpMethod;
+
+    @Schema(title = "Azure function URL")
+    @NotNull
+    protected Property<String> url;
+
+    @Schema(
+            title = "Http body",
+            description = "JSON body of the Azure function"
+    )
+    @Builder.Default
+    protected Property<Map<String, Object>> httpBody = Property.of(Collections.emptyMap());
+
+    @Schema(
+            title = "Max duration",
+            description = "The maximum duration the task should wait until the Azure Function completion."
+    )
+    @Builder.Default
+    @PluginProperty(dynamic = true)
+    protected Duration maxDuration = Duration.ofMinutes(60);
+
+    @Override
+    public HttpTrigger.Output run(RunContext runContext) throws Exception {
+        try (HttpClient client = this.client(runContext)) {
+            Mono<HttpResponse> mono = Mono.from(client.exchange(HttpRequest
+                    .create(
+                            HttpMethod.valueOf(httpMethod.as(runContext, String.class)),
+                            url.as(runContext, String.class)
+                    ).body(httpBody.asMap(runContext, String.class, Object.class)),
+                Argument.of(String.class))
+            );
+            HttpResponse result =  maxDuration != null ? mono.block(maxDuration) : mono.block();
+            String body = result != null &&  result.getBody().isPresent() ? (String) result.getBody().get() : "";
+
+            try {
+                return Output.builder()
+                    .repsonseBody(new JsonParser().parse(body))
+                    .build();
+            } catch (JsonSyntaxException e) {
+                return Output.builder()
+                    .repsonseBody(body)
+                    .build();
+            }
+        } catch (HttpClientResponseException e) {
+            throw new HttpClientResponseException(
+                    "Request failed '" + e.getStatus().getCode() + "' and body '" + e.getResponse().getBody(String.class).orElse("null") + "'",
+                    e,
+                    e.getResponse()
+            );
+        } catch (IllegalVariableEvaluationException | MalformedURLException | URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Builder
+    @Getter
+    static class Output implements io.kestra.core.models.tasks.Output {
+        @Schema(title = "Response body")
+        private Object repsonseBody;
+    }
+
+    protected HttpClient client(RunContext runContext) throws IllegalVariableEvaluationException, MalformedURLException, URISyntaxException {
+        MediaTypeCodecRegistry mediaTypeCodecRegistry = ((DefaultRunContext)runContext).getApplicationContext().getBean(MediaTypeCodecRegistry.class);
+
+        var httpConfig = new DefaultHttpClientConfiguration();
+        httpConfig.setMaxContentLength(Integer.MAX_VALUE);
+        httpConfig.setReadTimeout(HTTP_READ_TIMEOUT);
+
+        DefaultHttpClient client = (DefaultHttpClient) FACTORY.createClient(URI.create(url.as(runContext, String.class)).toURL(), httpConfig);
+        client.setMediaTypeCodecRegistry(mediaTypeCodecRegistry);
+
+        return client;
+    }
+}

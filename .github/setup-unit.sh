678fcbc740b61e6ef2fde195711c8456fb18d107
Ludovic DEHON <tchiot.ludo@gmail.com>
2025-02-06 00:17:05 +0100

    chore(cicd): move to reusable workflows on github actions

---
+++ .github/setup-unit.sh
@@ -0,0 +1,9 @@
+echo $AZURE_APPLICATION | base64 -d > src/test/resources/application-test.yml
+echo "kestra:" > src/test/resources/application-sp.yml
+echo "  variables:" >> src/test/resources/application-sp.yml
+echo "    globals:" >> src/test/resources/application-sp.yml
+echo "      azure:" >> src/test/resources/application-sp.yml
+echo "        sp:" >> src/test/resources/application-sp.yml
+echo "          tenant: $AZURE_TENANT_ID" >> src/test/resources/application-sp.yml
+echo "          username: $AZURE_CLIENT_ID" >> src/test/resources/application-sp.yml
+echo "          secret: $AZURE_CLIENT_SECRET" >> src/test/resources/application-sp.yml

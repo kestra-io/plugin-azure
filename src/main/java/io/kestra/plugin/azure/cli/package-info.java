ce88f9dfdf0653147c2d72269e0179fa52966684
brian.mulier <bmmulier@hotmail.fr>
2025-02-21 14:11:30 +0100

    fix: add package-info.java to cli package

---
+++ src/main/java/io/kestra/plugin/azure/cli/package-info.java
@@ -0,0 +1,7 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks to interact with Azure CLI. ",
+    categories = { PluginSubGroup.PluginCategory.CLOUD }
+)
+package io.kestra.plugin.azure.cli;
+
+import io.kestra.core.models.annotations.PluginSubGroup;

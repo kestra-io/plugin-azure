cba044f22659c9f321ba6def5af0e049d636b90f
Mathieu Gabelle <mgabelle@kestra.io>
2025-04-08 09:21:05 +0200

    chore: use @Nullable from Jakarata annotations

--- src/main/java/io/kestra/plugin/azure/batch/job/TaskService.java
+++ src/main/java/io/kestra/plugin/azure/batch/job/TaskService.java
@@ -22,7 +22,7 @@ import java.util.List;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
 import java.util.function.Consumer;
-import javax.annotation.Nullable;
+import jakarta.annotation.Nullable;
 
 public class TaskService {
     public static List<CloudTask> waitForTasksToComplete(

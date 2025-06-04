65648b3c2ac479210c85083fac4f6db1ace507a2
François Delbrayelle <fdelbrayelle@gmail.com>
2025-06-04 14:12:19 +0200

    fix(ci): missing connection-string

--- .github/setup-unit.sh
+++ .github/setup-unit.sh
@@ -7,3 +7,5 @@ echo "        sp:" >> src/test/resources/application-sp.yml
 echo "          tenant: $AZURE_TENANT_ID" >> src/test/resources/application-sp.yml
 echo "          username: $AZURE_CLIENT_ID" >> src/test/resources/application-sp.yml
 echo "          secret: $AZURE_CLIENT_SECRET" >> src/test/resources/application-sp.yml
+echo "        blobs:" >> src/test/resources/application-sp.yml
+echo "          connection-string: $AZURE_CONNECTION_STRING" >> src/test/resources/application-sp.yml

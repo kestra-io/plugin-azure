9dc2d8d31847562a1fdf9679b34d80752481098f
Steven Meek <42916669+ammeek@users.noreply.github.com>
2026-01-26 11:17:52 +0000

    feat(service_bus): introduce basic tasks (#236)

---
+++ src/main/java/io/kestra/plugin/azure/servicebus/Trigger.java
@@ -0,0 +1,131 @@
+package io.kestra.plugin.azure.servicebus;
+
+import com.azure.messaging.servicebus.models.ServiceBusReceiveMode;
+import com.azure.messaging.servicebus.models.SubQueue;
+import io.kestra.core.models.annotations.Example;
+import io.kestra.core.models.annotations.Plugin;
+import io.kestra.core.models.conditions.ConditionContext;
+import io.kestra.core.models.executions.Execution;
+import io.kestra.core.models.property.Property;
+import io.kestra.core.models.triggers.*;
+import io.kestra.core.runners.RunContext;
+import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import java.time.Duration;
+import java.util.Optional;
+
+import static io.kestra.plugin.azure.servicebus.AbstractServiceBusTask.*;
+import static io.kestra.plugin.azure.servicebus.Consume.*;
+
+@SuperBuilder
+@ToString
+@EqualsAndHashCode
+@Getter
+@NoArgsConstructor
+@Schema()
+@Plugin(
+    examples = {
+        @Example(
+            full = true,
+            code = """
+                id: service_bus_listen
+                namespace: company.team
+
+                tasks:
+                  - id: log_messages
+                    type: io.kestra.plugin.core.log.Log
+                    message: "Trigger body: {{trigger.body}}."
+
+                triggers:
+                  - id: watch
+                    type: io.kestra.plugin.azure.servicebus.Trigger
+                    maxReceiveDuration: PT30S
+                    maxMessages: 100
+                    queueName: your-queue-name
+                    tenantId: "{{ secret('AZURE_TENANT_ID') }}"
+                    clientId: "{{ secret('AZURE_CLIENT_ID') }}"
+                    clientSecret: "{{ secret('AZURE_CLIENT_SECRET') }}"
+                """
+        )
+    }
+)
+public class Trigger extends AbstractTrigger implements PollingTriggerInterface, TriggerOutput<Consume.Output> {
+    @Schema(description = QUEUE_NAME_DESCRIPTION)
+    private Property<String> queueName;
+
+    @Schema(description = TOPIC_NAME_DESCRIPTION)
+    private Property<String> topicName;
+
+    @Schema(description = CONNECTION_STRING_DESCRIPTION)
+    private Property<String> connectionString;
+
+    @Schema(description = SUBSCRIPTION_NAME_DESCRIPTION)
+    private Property<String> subscriptionName;
+
+    @Schema(description = RECEIVE_MODE_DESCRIPTION)
+    private Property<ServiceBusReceiveMode> receiveMode;
+
+    @Schema(description = SUB_QUEUE_DESCRIPTION)
+    private Property<SubQueue> subQueue;
+
+    @Builder.Default
+    @NotNull
+    @Schema(description = SERDE_TYPE_DESCRIPTION)
+    private Property<SerdeType> serdeType = Property.ofValue(SerdeType.STRING);
+
+    @Schema
+    private Property<String> tenantId;
+
+    @Schema
+    private Property<String> clientId;
+
+    @Schema
+    private Property<String> clientSecret;
+
+    @Schema
+    private Property<String> pemCertificate;
+
+    @Schema(description = MAX_MESSAGES_DESCRIPTION)
+    protected Property<Integer> maxMessages;
+
+    @NotNull
+    @Schema(description = MAX_RECEIVE_DURATION_DESCRIPTION)
+    protected Property<Duration> maxReceiveDuration;
+
+    @Builder.Default
+    private final Duration interval = Duration.ofSeconds(60);
+
+    @Override
+    public Optional<Execution> evaluate(ConditionContext conditionContext, TriggerContext context) throws Exception {
+        RunContext runContext = conditionContext.getRunContext();
+
+        Consume consume = Consume.builder()
+            .topicName(topicName)
+            .queueName(queueName)
+            .connectionString(connectionString)
+            .subscriptionName(subscriptionName)
+            .clientId(clientId)
+            .clientSecret(clientSecret)
+            .tenantId(tenantId)
+            .pemCertificate(pemCertificate)
+            .subQueue(subQueue)
+            .receiveMode(receiveMode)
+            .maxMessages(maxMessages)
+            .maxReceiveDuration(maxReceiveDuration)
+            .serdeType(serdeType)
+            .build();
+
+        Consume.Output run = consume.run(runContext);
+
+        if (run.getCount() == 0) {
+            return Optional.empty();
+        }
+
+        return Optional.ofNullable(
+            TriggerService.generateExecution(this, conditionContext, context, run)
+        );
+    }
+}
---
+++ src/test/java/io/kestra/plugin/azure/servicebus/ConsumeTest.java
@@ -0,0 +1,189 @@
+package io.kestra.plugin.azure.servicebus;
+
+import io.kestra.core.exceptions.IllegalVariableEvaluationException;
+import io.kestra.core.models.property.Property;
+import org.assertj.core.api.AbstractThrowableAssert;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.ResourceLock;
+
+import java.time.Duration;
+import java.time.Instant;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class ConsumeTest extends BaseServiceBusTest {
+    @Test
+    @ResourceLock("service-bus-comsumer-lock")
+    void shouldConsumePublishedTextMessage() throws Exception {
+        //region GIVEN
+        publishToTopic(Message.builder()
+            .body("example_message_body")
+            .timeToLive(Duration.ofSeconds(10))
+        );
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .build();
+        //endregion
+
+        //region WHEN
+        Consume.Output output = consume.run(runContextFactory.of());
+        //endregion
+
+        //region WHEN
+        assertThat(output.getCount()).isEqualTo(1);
+        //endregion
+    }
+
+    @Test
+    @ResourceLock("service-bus-comsumer-lock")
+    void shouldConsumePublishedJsonMessage() throws Exception {
+        //region GIVEN
+        publishToTopic(Message.builder()
+            .body("{\"message\":\"example_message_body\"}")
+            .timeToLive(Duration.ofSeconds(10))
+        );
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .serdeType(Property.ofValue(SerdeType.JSON))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .build();
+        //endregion
+
+        //region WHEN
+        Consume.Output output = consume.run(runContextFactory.of());
+        //endregion
+
+        //region WHEN
+        assertThat(output.getCount()).isEqualTo(1);
+        //endregion
+    }
+
+    @Test
+    @ResourceLock("service-bus-comsumer-lock")
+    void shouldReturnEmptyOutputWhenNoMessagesConsumed() throws Exception {
+        //region GIVEN
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .serdeType(Property.ofValue(SerdeType.JSON))
+            .build();
+        //endregion
+
+        //region WHEN
+        Consume.Output output = consume.run(runContextFactory.of());
+        //endregion
+
+        //region WHEN
+        assertThat(output.getCount()).isEqualTo(0);
+        //endregion
+    }
+
+    @Test
+    @ResourceLock("service-bus-comsumer-lock")
+    void shouldReturnEmptyOutputWhenNoMessagesConsumedAfterMaxDuration() throws Exception {
+        //region GIVEN
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .serdeType(Property.ofValue(SerdeType.JSON))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .build();
+        //endregion
+
+        //region WHEN
+        Instant start = Instant.now();
+        Consume.Output output = consume.run(runContextFactory.of());
+        //endregion
+
+        //region WHEN
+        assertThat(output.getCount()).isEqualTo(0);
+        assertThat(Instant.now().toEpochMilli() - start.toEpochMilli()).isGreaterThanOrEqualTo(1000);
+        //endregion
+    }
+
+    @Test
+    @ResourceLock("service-bus-comsumer-lock")
+    void shouldReturnOutputWhenAfterMaxMessagesReached() throws Exception {
+        //region GIVEN
+        publishToTopic(Message.builder()
+            .body("{\"message\":\"example_message_body\"}")
+            .timeToLive(Duration.ofSeconds(1))
+        );
+        publishToTopic(Message.builder()
+            .body("{\"message\":\"example_message_body\"}")
+            .timeToLive(Duration.ofSeconds(1))
+        );
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .serdeType(Property.ofValue(SerdeType.JSON))
+            .maxMessages(Property.ofValue(1))
+            .build();
+        //endregion
+
+        //region WHEN
+        Consume.Output output = consume.run(runContextFactory.of());
+        //endregion
+
+        //region WHEN
+        assertThat(output.getCount()).isEqualTo(1);
+        //endregion
+    }
+
+    @Test
+    void shouldThrowErrorWhenTopicAndQueueAreBothProvided() {
+        //region GIVEN
+        Consume consume = Consume.builder()
+            .topicName(Property.ofValue(topicName))
+            .queueName(Property.ofValue(queueName))
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .build();
+        //endregion
+
+        //region WHEN
+        AbstractThrowableAssert<?, ?> throwableAssert = assertThatThrownBy(() -> consume.run(
+            runContextFactory.of())
+        );
+        //endregion
+
+        //region WHEN
+        throwableAssert.isInstanceOf(IllegalVariableEvaluationException.class);
+        throwableAssert.hasMessageContaining("Exactly one of queueName or topicName must be specified.");
+        //endregion
+    }
+
+    @Test
+    void shouldThrowErrorWhenNeitherTopicAndQueueAreProvided() {
+        //region GIVEN
+        Consume consume = Consume.builder()
+            .connectionString(Property.ofValue(connectionString))
+            .subscriptionName(Property.ofValue(subscriptionName))
+            .maxReceiveDuration(Property.ofValue(Duration.ofSeconds(1)))
+            .build();
+        //endregion
+
+        //region WHEN
+        AbstractThrowableAssert<?, ?> throwableAssert = assertThatThrownBy(() -> consume.run(
+            runContextFactory.of())
+        );
+        //endregion
+
+        //region WHEN
+        throwableAssert.isInstanceOf(IllegalVariableEvaluationException.class);
+        throwableAssert.hasMessageContaining("Exactly one of queueName or topicName must be specified.");
+        //endregion
+    }
+}

af1ff1e7d0c34f75d65b31cd2b5f334d0380cc5a
Malay Dewangan <malaydewangan310@gmail.com>
2026-05-04 14:51:53 +0530

    chore(servicebus): add missing package-info

---
+++ src/main/java/io/kestra/plugin/azure/servicebus/package-info.java
@@ -0,0 +1,8 @@
+@PluginSubGroup(
+    description = "This sub-group of plugins contains tasks for using Azure Service Bus.\n" +
+        "Azure Service Bus is a fully managed enterprise message broker with message queues and publish-subscribe topics.",
+    categories = { PluginSubGroup.PluginCategory.CLOUD, PluginSubGroup.PluginCategory.DATA }
+)
+package io.kestra.plugin.azure.servicebus;
+
+import io.kestra.core.models.annotations.PluginSubGroup;

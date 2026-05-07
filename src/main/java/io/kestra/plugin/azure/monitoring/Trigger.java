8bbbaa980d99c449cfe1131ae7a0b7464e3cbace
Malay Dewangan <malaydewangan310@gmail.com>
2026-05-07 17:28:48 +0530

    fix: v2 compatibility

--- src/main/java/io/kestra/plugin/azure/monitoring/Trigger.java
+++ src/main/java/io/kestra/plugin/azure/monitoring/Trigger.java
@@ -43,7 +43,7 @@ import lombok.experimental.SuperBuilder;
                     tasks:
                       - id: log
                         type: io.kestra.plugin.core.log.Log
-                        message: "Datapoint: {{ json(item.value) }}"
+                        message: "Datapoint: {{ fromJson(item.value) }}"
 
                 triggers:
                   - id: watch
